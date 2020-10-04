fa8a90b2a8509f1320ba2359f1d98eda9177a6a7
Nicholas Jativa <nicholas.jativa@outlook.com>
2020-10-03 22:33:57 -0400

    Added interface and database logic for Recipient

---
+++ server/database_scripts/RecipientDatabaseScripts.ts
@@ -0,0 +1,12 @@
+export const RECIPIENT_TABLE: string = "recipient";
+
+export const CREATE_RECIPIENT_TABLE: string = `
+  CREATE TABLE ${RECIPIENT_TABLE} (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name TEXT,
+    address TEXT,
+    age INTEGER,
+    gender VARCHAR(6),
+    available BOOLEAN NOT NULL DEFAULT 0
+  )
+`;
---
+++ shared/interfaces/IRecipient.ts
@@ -0,0 +1,7 @@
+export interface IRecipient {
+  name: string;
+  address: string;
+  age: number;
+  gender: string;
+  available: boolean;
+}
